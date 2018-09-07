943f0656f3207ec7ab6e58a13efc83f7e4d58452
lym152898 <linyimin520812@gmail.com>
2018-09-07 08:48:22 +0800

    Adjust code style

--- src/sort/bubble-sort.ts
+++ src/sort/bubble-sort.ts
@@ -12,8 +12,8 @@ export class BubbleSort {
       for (let j = 0; j < arr.length - i - 1; j ++) {
         if (arr[j] > arr[j + 1]) {
           const temp: number = arr[j]
-          arr[j] = arr[j + 1]
-          arr[j + 1] = temp
+          arr  [j]           = arr[j + 1]
+          arr  [j + 1]       = temp
         }
       }
     }
