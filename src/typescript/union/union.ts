b6fff607c7b680eeb3b21936fc3b0ab1e4066d0e
linyimin-bupt <linyimin520812@gmail.com>
2019-01-16 21:52:12 +0800

    union implementation with geneal method

---
+++ src/typescript/union/union.ts
@@ -0,0 +1,38 @@
+export class Union {
+  private parent: number[]
+  private count: number
+  constructor (n: number) {
+    this.count = n
+    this.parent = new Array<number>(n)
+    for (let i = 0; i < n; i++) {
+      this.parent[i] = i
+    }
+  }
+  
+  public union (p: number, q: number) {
+    const pRoot = this.find(p)
+    const qRoot = this.find(q)
+    if (pRoot === qRoot) {
+      return
+    }
+    this.parent[pRoot] = qRoot
+  }
+  
+  public find (p: number): number {
+    if (p < 0 || p > this.count) {
+      throw new Error ('out of range')
+    }
+    while (p !== this.parent[p]) {
+      p = this.parent[p]
+    }
+    
+    return p
+  }
+  
+  
+  public isConnected (p: number, q: number): boolean {
+    const pRoot = this.find (p)
+    const qRoot = this.find (q)
+    return pRoot === qRoot
+  }
+}
