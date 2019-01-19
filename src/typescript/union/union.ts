3d1f901b5a445f2d0a7a30ced52243291bd51cea
linyimin-bupt <linyimin520812@gmail.com>
2019-01-19 23:55:05 +0800

    Implement insertOptimizationWithHeightOfSubtree function

--- src/typescript/union/union.ts
+++ src/typescript/union/union.ts
@@ -1,11 +1,16 @@
 export class Union {
   private parent: number[]
+  // 使用秩进行优化
+  private rank: number[]
   private count: number
   constructor (n: number) {
     this.count = n
     this.parent = new Array<number>(n)
+    this.rank   = new Array<number>(n)
     for (let i = 0; i < n; i++) {
       this.parent[i] = i
+      // 默认单节点对应的秩为1
+      this.rank[i] = 1
     }
   }
   
@@ -29,19 +34,32 @@ export class Union {
     return p
   }
   
-  public findOptimitionWithNumberOfNode (p: number): number {
+  public insertOptimizationWithHeightOfSubtree (p: number, q: number) {
+    const pRoot= this.find(p)
+    const qRoot= this.find(q)
+    if (pRoot === qRoot) {
+      return
+    }
+    if (this.rank[pRoot] > this.rank[qRoot]) {
+      this.parent[pRoot] = qRoot 
+      return
+    }
+    if (this.rank[pRoot] < this.rank[qRoot]) {
+      this.parent[qRoot] = pRoot
+      return
+    }
+    if (this.rank[pRoot] === this.rank[qRoot]) {
+      this.parent[qRoot] = pRoot
+      this.rank[qRoot] = this.rank[qRoot]++
+      return
+    }
+  }
+  
+  public insertOptimizationWithPathCompression (p: number): number {
     return 0
   }
   
-  public findOptiomitionWithHeightOfSubtree (p: number): number {
-    return 0
-  }
-  
-  public findOptinitionWithPathCompression (p: number): number {
-    return 0
-  }
-  
-  public findOptitionWithRecursive (p: number): number {
+  public findOptimizationWithRecursive (p: number): number {
     return 0
   }
   public isConnected (p: number, q: number): boolean {
