10e615c34b7fd87e570e025890f45af6db06e0d9
linyimin-bupt <linyimin520812@gmail.com>
2019-02-06 21:22:44 +0800

    implement bfs

---
+++ src/graph/bfs.spec.ts
@@ -0,0 +1,13 @@
+import test from 'blue-tape'
+import { readGraph } from './read-graph';
+import { BFS } from './bfs';
+
+test('bfs()', async t => {
+  const graph = await readGraph('./graph-example/graph1.txt', 'dense')
+  
+  const bfs: BFS = new BFS(graph)
+  
+  const result = bfs.bfs(0)
+  
+  t.equal(result, '0 1 2 5 3 4 ')
+})
---
+++ src/graph/bfs.ts
@@ -0,0 +1,40 @@
+import { DenseGraph } from "./dense-graph/dense-graph";
+import { SparseGraph } from "./sparse-graph/sparse-graph";
+import { DenseGraphIterator } from "./dense-graph/dense-graph-iterator";
+import { SparseGraphIterator } from "./sparse-graph/sparse-graph-iterator";
+
+export class BFS {
+  private graph: DenseGraph | SparseGraph
+  private isVisited: boolean[]
+  
+  private queue: number[]
+  
+  constructor (graph: DenseGraph | SparseGraph) {
+    this.graph = graph
+    this.queue = new Array<number>()
+    this.isVisited = new Array<boolean>(graph.V()).fill(false)
+  }
+  
+  public bfs(v: number): string {
+    this.isVisited[v] = true
+    this.queue.push(v)
+    let iterator: DenseGraphIterator | SparseGraphIterator
+    let result = ''
+    while(this.queue.length > 0) {
+      const vertex = this.queue.shift()!
+      result += vertex + ' '
+      if (this.graph instanceof DenseGraph) {
+        iterator = new DenseGraphIterator(this.graph, vertex)
+      } else {
+        iterator = new SparseGraphIterator(this.graph, vertex)      
+      }
+      for (let i = iterator.begin(); !iterator.end(); i = iterator.next()) {
+        if (! this.isVisited[i]) {
+          this.isVisited[i] = true
+          this.queue.push(i)
+        }
+      }
+    }
+    return result
+  }
+}
