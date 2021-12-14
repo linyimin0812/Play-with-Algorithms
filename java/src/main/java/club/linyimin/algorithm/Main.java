fb19ba243bfadc5a0b8ff02e409933e765d6019b
linyimin-bupt <linyimin520812@gmail.com>
2021-12-14 23:37:36 +0800

    init: add java language

---
+++ java/src/main/java/club/linyimin/algorithm/Main.java
@@ -0,0 +1,12 @@
+package club.linyimin.algorithm;
+
+/**
+ * @author yiminlin
+ * @date 2021/12/14 11:35 下午
+ * @description TODO:
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
