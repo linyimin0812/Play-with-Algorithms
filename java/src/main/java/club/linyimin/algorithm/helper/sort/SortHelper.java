89f0ef8ac6df2e99f146584e56619e70c207c663
linyimin-bupt <linyimin520812@gmail.com>
2021-12-15 00:39:28 +0800

    feat: add sort helper

---
+++ java/src/main/java/club/linyimin/algorithm/helper/sort/SortHelper.java
@@ -0,0 +1,157 @@
+package club.linyimin.algorithm.helper.sort;
+
+/**
+ * @author yiminlin
+ * @date 2021/12/15 12:02 am
+ * @description sort helper
+ **/
+public class SortHelper {
+
+    /**
+     * generate random array which contains n elements, and the value of element between left and right
+     * @param n number of element
+     * @param left the min value(inclusive)
+     * @param right the max value(exclusive)
+     * @return a random array
+     */
+    public static int[] generateRandomArray(int n, int left, int right) {
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (int) (Math.random() * (right - left) + left);
+        }
+
+        return arr;
+    }
+
+    /**
+     * generate random array which contains n elements, and the value of element between left and right
+     * @param n number of element
+     * @param left the min value(inclusive)
+     * @param right the max value(exclusive)
+     * @return a random array
+     */
+    public static long[] generateRandomArray(int n, long left, long right) {
+
+        long[] arr = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (long) (Math.random() * (right - left) + left);
+        }
+
+        return arr;
+    }
+
+    /**
+     * generate random array which contains n elements, and the value of element between left and right
+     * @param n number of element
+     * @param left the min value(inclusive)
+     * @param right the max value(exclusive)
+     * @return a random array
+     */
+    public static float[] generateRandomArray(int n, float left, float right) {
+
+        float[] arr = new float[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (float) (Math.random() * (right - left) + left);
+        }
+
+        return arr;
+    }
+
+    /**
+     * generate random array which contains n elements, and the value of element between left and right
+     * @param n number of element
+     * @param left the min value(inclusive)
+     * @param right the max value(exclusive)
+     * @return a random array
+     */
+    public static double[] generateRandomArray(int n, double left, double right) {
+
+        double[] arr = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (Math.random() * (right - left) + left);
+        }
+
+        return arr;
+    }
+
+
+    public static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSorted(long[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSorted(float[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSorted(double[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void print(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (int j : arr) {
+            sb.append(j).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void print(long[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (long l : arr) {
+            sb.append(l).append(" ");
+        }
+        sb.append(arr[arr.length - 1]);
+        System.out.println(sb.toString());
+    }
+
+    public static void print(float[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (float v : arr) {
+            sb.append(v).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void print(double[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (double v : arr) {
+            sb.append(v).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+
+
+    public static void main(String[] args) {
+        print(generateRandomArray(1000, 0, 100));
+    }
+}
